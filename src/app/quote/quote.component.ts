7526057af9765878847073a63c8fd66d99e48476
clintonoliera <clintonoliera391@gmail.com>
2020-06-13 22:59:22 +0300

    added quote names and their descriptions

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,11 +1,17 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Quote} from '../quote'
 @Component({
   selector: 'app-quote',
   templateUrl: './quote.component.html',
   styleUrls: ['./quote.component.css']
 })
 export class QuoteComponent implements OnInit {
+
+   quotes: Quote[] = [
+   new Quote(1,'Motivation','Push yourself, because no one else is going to do it for you..'),
+   new Quote(2, "Perseverance", "If you want to see the rainbow you have to put up with the rain.."),
+   new Quote(3,'Inspiration','He was the light at the end of the road..')
+ ]
 
   constructor() { }
 
