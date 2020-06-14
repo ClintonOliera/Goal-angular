316533a08aec8027426bfe6fc83078c0bf99747d
clintonoliera <clintonoliera391@gmail.com>
2020-06-14 10:26:15 +0300

    added public constructors for author date posted upvotes and downvotes

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -2,7 +2,15 @@ export class Quote {
 
 showDescription:boolean;
 
- constructor(public id: number, public name: string, public description: string){
+ constructor(
+   public id: number, 
+   public name: string, 
+   public description: string, 
+   public author:string, 
+   public postDate:Date, 
+   public upvotes: number, 
+   public downvotes: number
+   ){
       this.showDescription=false
     };
-}
+};
