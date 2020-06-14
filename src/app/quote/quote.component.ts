ff414704174f78f81d7c7dfe00a47944ed9746bd
clintonoliera <clintonoliera391@gmail.com>
2020-06-14 13:05:19 +0300

    added a delete function to delete a quote

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -8,10 +8,20 @@ import { Quote} from '../quote'
 export class QuoteComponent implements OnInit {
 
    quotes: Quote[] = [
-   new Quote(1,'Motivation','Push yourself, because no one else is going to do it for you..'),
-   new Quote(2, "Perseverance", "If you want to see the rainbow you have to put up with the rain.."),
-   new Quote(3,'Inspiration','He was the light at the end of the road..,Sometimes later becomes never. ...')
+   new Quote(1,'Motivation','Push yourself, because no one else is going to do it for you..', 'martin lutherking', new Date(2020,5,14), 0,0),
+   new Quote(2, "Perseverance", "If you want to see the rainbow you have to put up with the rain..", 'Angelo', new Date(2020,5,14), 0,0),
+   new Quote(3,'Inspiration','He was the light at the end of the road..,Sometimes later becomes never. ...', 'clinton391', new Date(2020,5,14), 0,0)
  ]
+
+   deleteQuote(isComplete, index){
+     if(isComplete){
+       let toDelete = confirm(`Are you sure you want to delete ${this.quotes[index].name}..?`)
+
+       if(toDelete){
+         this.quotes.splice(index, 1);
+       }
+     }
+   }
 
   toggleDetails(index){
     this.quotes[index].showDescription =! this.quotes[index].showDescription;
